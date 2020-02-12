b81f35d505e0f63f0158b88cdd01e81cfdd54a40
sotomaque <sotomaque@gmail.com>
2020-02-11 21:52:00 -0800

    added edit and delete mutations for user objects

--- schema/schema.js
+++ schema/schema.js
@@ -93,6 +93,20 @@ const mutation = new GraphQLObjectType({
                 return axios.delete(`http://localhost:3000/users/${id}`)
                     .then(resp => resp.data);
             }
+        },
+
+        editUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) },
+                firstName: { type: GraphQLString },
+                age: { type: GraphQLInt }, 
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, { id, firstName, age, companyId }) {
+                return axios.patch(`http://localhost:3000/users/${id}`, { firstName, age, companyId })
+                    .then(resp => resp.data);
+            }
         }
     }
 });
